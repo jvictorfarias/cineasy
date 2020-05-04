a016602c27c479cdfbdd7691a71b52ca17609878
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-05-04 02:02:57 -0300

    dependency: added cors to api

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 
@@ -8,6 +9,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
